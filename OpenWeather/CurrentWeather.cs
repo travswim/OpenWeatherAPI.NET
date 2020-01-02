91ad4e94ca7512005b1a85b4b062c31c1d3f1541
Travis Booth <travswim@gmail.com>
2020-01-01 17:57:29 -0800

    Add project files.

---
+++ OpenWeather/CurrentWeather.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+// TODO: Retrieve weather as is
+// TODO: Retrieve weather in different units
+// TODO: Retrieve weather in type or string format
+// TODO: Retrieve data from gps, name, or IP address
+namespace OpenWeather
+{
+    public class CurrentWeather
+    {
+        public static string WEA = "https://api.openweathermap.org/data/2.5/weather?q={0},{1}&appid={2}";
+        //[JsonPropertyName("weather")]
+        //public IList<Dictionary<string, string>> Weather { get; set; }
+        [JsonPropertyName("base")]
+        public string Base { get; set; }
+        [JsonPropertyName("main")]
+        public Dictionary<string, double> Main { get; set; }
+        [JsonPropertyName("wind")]
+        public Dictionary<string, double> Wind { get; set; }
+        [JsonPropertyName("clouds")]
+        public Dictionary<string, int> Clouds { get; set; }
+
+        
+        [JsonPropertyName("dt")]
+        public int DT { get; set; }
+            
+        [JsonPropertyName("sys")]
+        public WeatherSystem Sys { get; set; }
+
+        [JsonPropertyName("timezone")]
+        public int TimeZone { get; set; }
+
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("name")]
+        public string CityName { get; set; }
+        [JsonPropertyName("cod")]
+        public int Code { get; set; }
+
+        public class WeatherSystem
+        {
+            public int type { get; set; }
+            public int id { get; set; }
+            public float message { get; set; }
+            public string country { get; set; }
+            public int sunrise { get; set; }
+            public int sunset { get; set; }
+        }
+
+
+
+    }
+}
---
+++ OpenWeatherAPI.NET/Program.cs
@@ -0,0 +1,43 @@
+﻿using OpenWeather;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace OpenWeatherAPI.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CODE = "16203ed9d9f4e7286a07ff4aef754f77";
+            string CityName = "West Vancouver";
+            string CountryCode = "CA";
+            //Console.WriteLine(string.Format(CurrentWeather.WEA, CityName.Replace(" ", "%20"), CountryCode, CODE));
+            string jsonString = GetJSON(string.Format(CurrentWeather.WEA, CityName.Replace(" ", "%20"), CountryCode, CODE));
+            CurrentWeather currentWeather = JsonSerializer.Deserialize<CurrentWeather>(jsonString);
+            Console.WriteLine(currentWeather.Main["temp"]);
+            Console.WriteLine(currentWeather.DT);
+            Console.WriteLine(currentWeather.TimeZone);
+            Console.WriteLine(currentWeather.Sys.sunrise);
+            Console.WriteLine(currentWeather.Sys.sunset);
+
+        }
+
+        /// <summary>
+        /// Retrieves a JSON string from a web API given a valid URL
+        /// </summary>
+        /// <param name="address">http or https URL</param>
+        /// <returns>A JSON string</returns>
+        public static string GetJSON(string address)
+        {
+            // Initiate a webclient and get the string returned
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(address);
+
+            //Console.WriteLine(reply);
+            return reply;
+
+        }
+    }
+}
